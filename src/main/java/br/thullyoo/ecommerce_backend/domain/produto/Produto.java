59db4113d2a64f6454181789051ed37deb74315a
Thullyoo <thullyofilho@gmail.com>
2024-11-15 22:57:44 -0300

    feat: adiciona produto entity

---
+++ src/main/java/br/thullyoo/ecommerce_backend/domain/produto/Produto.java
@@ -0,0 +1,69 @@
+package br.thullyoo.ecommerce_backend.domain.produto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TB_PRODUTOS")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @Column(name = "produto_id")
+    private Long id;
+
+    private String nome;
+
+    private Double valor;
+
+    private String url_imagem;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getUrl_imagem() {
+        return url_imagem;
+    }
+
+    public void setUrl_imagem(String url_imagem) {
+        this.url_imagem = url_imagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
