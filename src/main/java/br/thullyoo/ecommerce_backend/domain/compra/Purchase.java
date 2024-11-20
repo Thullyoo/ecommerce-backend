6fd1fe671b4d3420e639ce7913164b117cc2eff3
Thullyoo <thullyofilho@gmail.com>
2024-11-20 16:10:46 -0300

    feat: add purchase entity

---
+++ src/main/java/br/thullyoo/ecommerce_backend/domain/compra/Purchase.java
@@ -0,0 +1,53 @@
+package br.thullyoo.ecommerce_backend.domain.compra;
+
+import br.thullyoo.ecommerce_backend.domain.produto.Product;
+import br.thullyoo.ecommerce_backend.domain.user.User;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "TB_COMPRAS")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "purchase_id")
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "purchase_product",
+            joinColumns = @JoinColumn(name = "purchase_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
