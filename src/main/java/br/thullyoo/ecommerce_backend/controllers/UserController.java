59be612ed8d0cbbc3e2d9b5f3a8ebcf10722aef3
Thullyoo <thullyofilho@gmail.com>
2024-11-20 17:14:57 -0300

    feat: add endpoint for create user

---
+++ src/main/java/br/thullyoo/ecommerce_backend/controllers/UserController.java
@@ -0,0 +1,29 @@
+package br.thullyoo.ecommerce_backend.controllers;
+
+import br.thullyoo.ecommerce_backend.domain.user.User;
+import br.thullyoo.ecommerce_backend.domain.user.UserRequest;
+import br.thullyoo.ecommerce_backend.services.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(@RequestBody UserRequest userRequest){
+        User user = userService.registerUser(userRequest);
+        return ResponseEntity.ok().body(user);
+    }
+}
+
