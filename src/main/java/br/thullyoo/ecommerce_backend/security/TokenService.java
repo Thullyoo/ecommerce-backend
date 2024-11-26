f8ad341ee569b5da11cf5c5279618bf8691c2523
Thullyoo <thullyofilho@gmail.com>
2024-11-26 13:46:12 -0300

    feat: add token service for get token

---
+++ src/main/java/br/thullyoo/ecommerce_backend/security/TokenService.java
@@ -0,0 +1,45 @@
+package br.thullyoo.ecommerce_backend.security;
+
+import br.thullyoo.ecommerce_backend.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
+    public String generateToken(Authentication authentication){
+
+        Instant issuedAt = Instant.now();
+
+        var expirationTime = issuedAt.plusSeconds(120);
+
+        var scopes = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("spring-security")
+                .subject(authentication.getName())
+                .claim("scope", scopes)
+                .issuedAt(issuedAt)
+                .expiresAt(expirationTime)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+    }
+
+}
