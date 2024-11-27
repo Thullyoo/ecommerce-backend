de6b1bc8de18ecc0c9ceb0cb1ef3d006b90910e1
Thullyoo <thullyofilho@gmail.com>
2024-11-27 12:00:21 -0300

    feat: add method to login

--- src/main/java/br/thullyoo/ecommerce_backend/services/UserService.java
+++ src/main/java/br/thullyoo/ecommerce_backend/services/UserService.java
@@ -6,6 +6,10 @@ import br.thullyoo.ecommerce_backend.domain.user.User;
 import br.thullyoo.ecommerce_backend.domain.user.UserMapper;
 import br.thullyoo.ecommerce_backend.domain.user.UserRequest;
 import br.thullyoo.ecommerce_backend.repositories.UserRepository;
+import br.thullyoo.ecommerce_backend.security.AuthService;
+import br.thullyoo.ecommerce_backend.security.TokenDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,12 +24,16 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+    private final AuthService authService;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, AuthService authService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
+        this.authService = authService;
     }
 
+    @Transactional
     public User registerUser(UserRequest userRequest){
         var user = userMapper.toUser(userRequest);
         user.setProducts(new ArrayList<Product>());
@@ -33,4 +41,11 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+    public TokenDTO login(Authentication authentication){
+        String token = authService.authenticate(authentication);
+        return new TokenDTO(token, 120L);
+    }
+
+
 }
