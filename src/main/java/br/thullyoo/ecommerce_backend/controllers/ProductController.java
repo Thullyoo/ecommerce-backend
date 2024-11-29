9fbbc5df1cf9f6e5f69230d76edb1bbc248829a2
Thullyoo <thullyofilho@gmail.com>
2024-11-29 14:59:13 -0300

    feat: add jwt on parameter

--- src/main/java/br/thullyoo/ecommerce_backend/controllers/ProductController.java
+++ src/main/java/br/thullyoo/ecommerce_backend/controllers/ProductController.java
@@ -5,39 +5,41 @@ import br.thullyoo.ecommerce_backend.domain.product.ProductRequest;
 import br.thullyoo.ecommerce_backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Product> registrarProduto(@RequestBody ProductRequest productRequest){
-        var produto = productService.registerProduct(productRequest);
+    public ResponseEntity<Product> registerProduct(@RequestBody ProductRequest productRequest, @AuthenticationPrincipal Jwt jwt){
+        var produto = productService.registerProduct(productRequest, jwt);
         return ResponseEntity.ok().body(produto);
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> listarProdutos(){
+    public ResponseEntity<List<Product>> listProduct(){
         return ResponseEntity.ok().body(productService.listProducts());
     }
 
     @PutMapping("/{produto_id}")
-    public ResponseEntity<Product> editarProduto(@PathVariable("produto_id")  UUID produto_id, @RequestBody ProductRequest productRequest){
-        var produtoLista = productService.editProduct(produto_id, productRequest);
+    public ResponseEntity<Product> editProduct(@PathVariable("product_id")  UUID product_id, @RequestBody ProductRequest productRequest, @AuthenticationPrincipal Jwt jwt){
+        var produtoLista = productService.editProduct(product_id, productRequest, jwt);
 
         return ResponseEntity.ok().body(produtoLista);
     }
 
-    @DeleteMapping("/{user_id}/{product_id}")
-    public ResponseEntity<Void> excluirProduto(@PathVariable("product_id") UUID product_id, @PathVariable("user_id") UUID user_id){
-        productService.deleteProduct(product_id, user_id);
+    @DeleteMapping("/{user_id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("user_id") UUID user_id, @AuthenticationPrincipal Jwt jwt){
+        productService.deleteProduct(jwt, user_id);
 
         return ResponseEntity.noContent().build();
     }
