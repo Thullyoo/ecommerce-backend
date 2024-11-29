e2458ea3db8c9fdc2ed1b4d47cc9077eacf53faa
Thullyoo <thullyofilho@gmail.com>
2024-11-29 14:36:21 -0300

    feat: add annotation for toProduct

--- src/main/java/br/thullyoo/ecommerce_backend/domain/product/ProductMapper.java
+++ src/main/java/br/thullyoo/ecommerce_backend/domain/product/ProductMapper.java
@@ -1,10 +1,15 @@
 package br.thullyoo.ecommerce_backend.domain.product;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "value", target = "value")
+    @Mapping(source = "url_image", target = "url_image")
     Product toProduct(ProductRequest productRequest);
 
 }
