5ff6e5869a32a94b4a67f186adea08105af685dd
Thullyoo <thullyofilho@gmail.com>
2024-12-02 12:58:08 -0300

    fix: adjust cors config

--- src/main/java/br/thullyoo/ecommerce_backend/config/SecurityConfig.java
+++ src/main/java/br/thullyoo/ecommerce_backend/config/SecurityConfig.java
@@ -20,6 +20,10 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -37,19 +41,21 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors().configurationSource(corsConfigurationSource()) // Habilita o CORS aqui
+                .and()
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests( auth ->{
+                .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/user/login").permitAll()
-                    .requestMatchers("/user/register").permitAll()
-                    .anyRequest().authenticated();
+                            .requestMatchers("/user/register").permitAll()
+                            .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(conf -> {
-                    conf.jwt( jwt ->{
+                    conf.jwt(jwt -> {
                         jwt.decoder(this.jwtDecoder());
                     });
                 })
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->{
+                .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .build();
@@ -70,5 +76,18 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
         JWKSource<SecurityContext> jwkSet = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwkSet);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
