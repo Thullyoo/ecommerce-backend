676e34121cdf97a40fc5efa236c0d73171541b4b
Thullyoo <thullyofilho@gmail.com>
2024-12-03 14:26:01 -0300

    fix: adjusts new param required

--- src/main/java/br/thullyoo/ecommerce_backend/controllers/ProductController.java
+++ src/main/java/br/thullyoo/ecommerce_backend/controllers/ProductController.java
@@ -4,11 +4,14 @@ import br.thullyoo.ecommerce_backend.domain.product.Product;
 import br.thullyoo.ecommerce_backend.domain.product.ProductRequest;
 import br.thullyoo.ecommerce_backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,8 +22,22 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping
-    public ResponseEntity<Product> registerProduct(@RequestBody ProductRequest productRequest, @AuthenticationPrincipal Jwt jwt){
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Product> registerProduct(
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("value") Double value,
+            @RequestParam("quantity") Long quantity,
+            @RequestParam("image") MultipartFile image
+            , @AuthenticationPrincipal Jwt jwt)  throws IOException {
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setName(name);
+        productRequest.setDescription(description);
+        productRequest.setValue(value);
+        productRequest.setQuantity(quantity);
+        productRequest.setImage(image);
+
         var produto = productService.registerProduct(productRequest, jwt);
         return ResponseEntity.ok().body(produto);
     }
