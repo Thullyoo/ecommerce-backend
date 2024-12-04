cd2e8bbbf7c8128daca24fc7fcd657c76d0a4929
Thullyoo <thullyofilho@gmail.com>
2024-12-04 14:52:09 -0300

    feat: add method to find product by name

--- src/main/java/br/thullyoo/ecommerce_backend/repositories/ProductRepository.java
+++ src/main/java/br/thullyoo/ecommerce_backend/repositories/ProductRepository.java
@@ -2,10 +2,18 @@ package br.thullyoo.ecommerce_backend.repositories;
 
 import br.thullyoo.ecommerce_backend.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Query(value = "SELECT * FROM TB_PRODUCTS WHERE LOWER(name) LIKE LOWER(CONCAT('%', :name, '%'))", nativeQuery = true)
+    List<Product> findByName(@Param("name") String name);
+
+
 }
