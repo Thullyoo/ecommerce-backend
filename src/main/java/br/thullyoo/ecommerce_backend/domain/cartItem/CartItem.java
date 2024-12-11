7ba62d42c6a3a9bdfbd12ae9f877ccb464f7c79e
Thullyoo <thullyofilho@gmail.com>
2024-12-11 14:07:39 -0300

    feat: add entity cart item

---
+++ src/main/java/br/thullyoo/ecommerce_backend/domain/cartItem/CartItem.java
@@ -0,0 +1,57 @@
+package br.thullyoo.ecommerce_backend.domain.cartItem;
+
+import br.thullyoo.ecommerce_backend.domain.cart.Cart;
+import br.thullyoo.ecommerce_backend.domain.product.Product;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_CART-ITEM")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
