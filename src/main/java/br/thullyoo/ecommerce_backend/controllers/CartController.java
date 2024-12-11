9d1560401badcd07ea8a10832701eb01948cc28e
Thullyoo <thullyofilho@gmail.com>
2024-12-11 14:21:23 -0300

    feat: add endpoint to remove item cart

--- src/main/java/br/thullyoo/ecommerce_backend/controllers/CartController.java
+++ src/main/java/br/thullyoo/ecommerce_backend/controllers/CartController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart")
@@ -18,10 +15,15 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @PostMapping
+    @PostMapping("/add")
     public ResponseEntity<Void> addItemCart(@RequestBody CardItemRequest cardItem,@AuthenticationPrincipal Jwt jwt){
-        cartService.addCartItem(cardItem, jwt);
+        this.cartService.addCartItem(cardItem, jwt);
         return ResponseEntity.noContent().build();
     }
 
+    @PostMapping("/remove/{itemCart_id}")
+    public ResponseEntity<Void> removeItemCart(@PathVariable("itemCart_id") Long itemCart_id, @AuthenticationPrincipal Jwt jwt){
+        this.cartService.removeItemCart(itemCart_id, jwt);
+        return ResponseEntity.noContent().build();
+    }
 }
